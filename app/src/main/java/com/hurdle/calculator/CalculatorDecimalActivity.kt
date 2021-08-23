3332a3e56e3674fcb6a96453f76f195000fa58a0
HyukJin Kwon <insort@naver.com>
2021-08-23 17:11:38 +0900

    숫자버튼 입력처리

--- app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
+++ app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
@@ -12,18 +12,37 @@ class CalculatorDecimalActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCalculatorDecimalBinding
 
+    private var isFirstInput = true
+    private var resultNumber: Long = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorDecimalBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
+    // 최대 길이 설정이 필요합니다.
+    // 정수형 계산기이므로 0부터 입력하는 방법을 예외처리 해야합니다.
     fun numberClick(view: View) {
         Log.d(TAG, "numberClick: ${view.tag}")
+        if (isFirstInput) {
+            binding.calculatorInputTextView.text = view.tag.toString()
+            isFirstInput = false
+        } else {
+            binding.calculatorInputTextView.append(view.tag.toString())
+        }
     }
 
+    //
     fun operateEvent(view: View) {
-        Log.d(TAG, "operateEvent: ${view.tag}")
+        resultNumber = binding.calculatorInputTextView.text.toString().toLong()
+
+        Log.d(TAG, "operateEvent: ${view.tag}, resultNumber: ${resultNumber}")
+
+
+        when (view.tag.toString()) {
+
+        }
     }
 
     fun resultEvent(view: View) {
@@ -32,5 +51,10 @@ class CalculatorDecimalActivity : AppCompatActivity() {
 
     fun clearEvent(view: View) {
         Log.d(TAG, "clearEvent: ${view.tag}")
+
+        binding.calculatorInputTextView.text = "0"
+        binding.calculatorRecordTextView.text = null
+        isFirstInput = true
+        resultNumber = 0L
     }
 }
