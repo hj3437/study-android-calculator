d0306f6b6440bc2dc23f9e520ea24b0a2f8115fb
HyukJin Kwon <insort@naver.com>
2021-08-23 10:26:47 +0900

    계산기능 추가

--- app/src/main/java/com/hurdle/calculator/MainActivity.kt
+++ app/src/main/java/com/hurdle/calculator/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import com.hurdle.calculator.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var operator = "+"
+    private var resultNumber: Double = 0.0
     private var isFirstInput = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,21 +23,74 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // init
-        binding.mainInputTextView.text = "0"
-        binding.mainRecordTextView.text = "0"
+        binding.mainInputTextView.text = ""
+        binding.mainRecordTextView.text = ""
     }
 
     fun onButtonClick(view: View) {
-        // val button = findViewById<Button>(view.id)
+        val btnTag = view.tag.toString()
 
+        if (btnTag == getString(R.string.btn_plus) ||
+            btnTag == getString(R.string.btn_minus) ||
+            btnTag == getString(R.string.btn_multiply) ||
+            btnTag == getString(R.string.btn_divide)
+        ) {
+            try {
+                val inputNumber = binding.mainInputTextView.text.toString().toDouble()
 
-        if (isFirstInput) {
-            binding.mainInputTextView.text = view.tag.toString()
-            isFirstInput = false
-        } else {
-            binding.mainInputTextView.append(view.tag.toString())
+                when (operator) {
+                    getString(R.string.btn_plus) -> {
+                        Log.d(TAG, "onButtonClick: btn_plus")
+                        resultNumber += inputNumber
+                    }
+                    getString(R.string.btn_minus) -> {
+                        Log.d(TAG, "onButtonClick: btn_minus")
+                        resultNumber -= inputNumber
+                    }
+                    getString(R.string.btn_multiply) -> {
+                        Log.d(TAG, "onButtonClick: btn_multiply")
+                        resultNumber *= inputNumber
+                    }
+                    getString(R.string.btn_divide) -> {
+                        Log.d(TAG, "onButtonClick: btn_divide")
+                        resultNumber /= inputNumber
+                    }
+                }
+
+                binding.mainInputTextView.text = resultNumber.toString()
+                isFirstInput = true
+                operator = view.tag.toString()
+
+                binding.mainRecordTextView.append("$inputNumber $operator ")
+
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+
+            Log.d(TAG, "$resultNumber")
+
+        } else if (btnTag == getString(R.string.btn_one) ||
+            btnTag == getString(R.string.btn_two) ||
+            btnTag == getString(R.string.btn_three) ||
+            btnTag == getString(R.string.btn_four) ||
+            btnTag == getString(R.string.btn_five) ||
+            btnTag == getString(R.string.btn_six) ||
+            btnTag == getString(R.string.btn_seven) ||
+            btnTag == getString(R.string.btn_eight) ||
+            btnTag == getString(R.string.btn_nine) ||
+            btnTag == getString(R.string.btn_zero) ||
+            btnTag == getString(R.string.btn_open_parentheses) ||
+            btnTag == getString(R.string.btn_close_parentheses) ||
+            btnTag == getString(R.string.btn_dot)
+        ) {
+
+            if (isFirstInput) {
+                binding.mainInputTextView.text = view.tag.toString()
+                isFirstInput = false
+            } else {
+                binding.mainInputTextView.append(view.tag.toString())
+            }
         }
-
-        Log.d(TAG, "BTN >>> ${view.tag  }")
+        // Log.d(TAG, "BTN >>> ${view.tag}")
     }
 }
