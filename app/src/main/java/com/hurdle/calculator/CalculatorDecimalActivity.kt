eeba04d2566f071e27ff544c0ce66617b0cba580
HyukJin Kwon <insort@naver.com>
2021-08-23 17:00:20 +0900

    버튼이벤트 함수 추가

--- app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
+++ app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
@@ -2,10 +2,13 @@ package com.hurdle.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.hurdle.calculator.databinding.ActivityCalculatorDecimalBinding
 import com.hurdle.calculator.databinding.ActivityStudyBinding
 
 class CalculatorDecimalActivity : AppCompatActivity() {
+    private val TAG = CalculatorDecimalActivity::class.java.simpleName
 
     private lateinit var binding: ActivityCalculatorDecimalBinding
 
@@ -13,5 +16,21 @@ class CalculatorDecimalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorDecimalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun numberClick(view: View) {
+        Log.d(TAG, "numberClick: ${view.tag}")
+    }
+
+    fun operateEvent(view: View) {
+        Log.d(TAG, "operateEvent: ${view.tag}")
+    }
+
+    fun resultEvent(view: View) {
+        Log.d(TAG, "resultEvent: ${view.tag}")
+    }
+
+    fun clearEvent(view: View) {
+        Log.d(TAG, "clearEvent: ${view.tag}")
     }
 }
