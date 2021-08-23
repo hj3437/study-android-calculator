351c732204a7631cd46f89b7863f53cf7a65ba70
HyukJin Kwon <insort@naver.com>
2021-08-23 16:47:23 +0900

    binding 추가

--- app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
+++ app/src/main/java/com/hurdle/calculator/CalculatorDecimalActivity.kt
@@ -2,10 +2,16 @@ package com.hurdle.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hurdle.calculator.databinding.ActivityCalculatorDecimalBinding
+import com.hurdle.calculator.databinding.ActivityStudyBinding
 
 class CalculatorDecimalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCalculatorDecimalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calculator_decimal)
+        binding = ActivityCalculatorDecimalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
